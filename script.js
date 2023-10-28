56a7464b8717790cbc6b242a3f773e8fc13c5592
Alexander <alex-Xio@users.noreply.github.com>
2023-10-28 12:31:54 +0200

    project done

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('hello world');
+const grid = document.querySelector('div#grid');
+setGridSize(10);
+function setGridSize(ratio) {
+	document.documentElement.style.setProperty('--grid-ratio-number', ratio);
+	while (grid.firstChild) {
+		grid.removeChild(grid.firstChild);
+	}
+	for (let i = 0; i < ratio ** 2; i++) {
+		let pixel = document.createElement('div');
+		pixel.classList.add('pixel');
+		addMarkEvent(pixel);
+		grid.appendChild(pixel);
+	}
+}
+function gridSizePrompt(message = 'Input size of the grid:') {
+	let userInput = prompt(message);
+	if (Number.isInteger(+userInput) && userInput > 0) {
+		if (userInput > 100) {
+			gridSizePrompt('Please enter a value that is less than (or equals) 100:');
+		} else {
+			setGridSize(userInput);
+		}
+	} else {
+		gridSizePrompt('Invalid value! Please try again:');
+	}
+}
+
+function addMarkEvent(element) {
+	element.onmouseenter = (e) => {
+		if (!e.target.classList.contains('marked')) {
+			e.target.classList.add('marked');
+		}
+	};
+}
